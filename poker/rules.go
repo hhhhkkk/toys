f694db73f3ce6ee15bee073f3fc0da1868708f3f
hhhhkkk <2387238381@qq.com>
2026-07-24 19:06:25 +0800

    验证飞机

--- poker/rules.go
+++ poker/rules.go
@@ -123,6 +123,87 @@ type Plane struct {
 	subRule Three
 }
 
+var _ Rule = (*Plane)(nil)
+
+func (p Plane) Check(c *Collection) (ret bool) {
+	ret = false
+	// 顺子通常是 n 个 三带一、 n 个三带二、n 个三不带
+	l := len(c.pais)
+	if l > 20 || l < 6 {
+		return
+	}
+
+	c.Sort()
+	// 3 不带：6、9、12、15、18
+	// 3 带 1：8、12、16、20
+	// 3 带 2：10、15、20
+
+	// 统计出来 3 个张数 机体，如果有一个是大于 3 的，就不是
+	// 判断所有 3 的个数是否是连续的
+	// 判断翅膀， 其应该是 机体个数 相等或者是 2n,否则报错
+	// 如果是 2n , 则判断是否都是对， 否则报错
+	m := make(map[int]int)
+
+	for _, v := range c.pais {
+		if _, ok := m[int(v.Number)]; !ok {
+			m[int(v.Number)] = 1
+		} else {
+			m[int(v.Number)] += 1
+		}
+	}
+
+	mainPais := make([]pai, 0)
+	attchPais := make([]pai, 0)
+	// 有超过 3 的
+	for k, v := range m {
+		if v > 3 {
+			return
+		}
+		mainPai := slices.DeleteFunc(c.pais, func(p pai) bool {
+			return int(p.Number) == k
+		})
+		if v == 3 {
+			mainPais = append(mainPais, mainPai...)
+			continue
+		}
+		attchPais = append(attchPais, mainPai...)
+	}
+
+	// 所有主牌都是连续的
+	basePai := mainPais[0]
+	for _, v := range mainPais[1:] {
+		if v.Number != basePai.Number && v.Number != basePai.Number+1 {
+			return
+		}
+	}
+
+	ml := len(m)
+	al := len(attchPais)
+	if al%ml != 0 {
+		return false
+	}
+
+	// 都是对
+	// 都是单无需验证
+	if al > ml {
+		tmpC := NewCollection()
+		tmpC.pais = attchPais
+		tmpC.Sort()
+
+		chunk := slices.Chunk(tmpC.pais, 2)
+		dui := Dui{}
+		for v := range chunk {
+			tc := NewCollection()
+			tc.pais = v
+			if dui.Check(tc) != true {
+				return false
+			}
+		}
+	}
+	ret = true
+	return ret
+}
+
 // Straight 顺子.
 type Straight struct{}
 
